88c0c71bcf4245f0adb7972c8e486b4939aa5fdc
Alfons Caroles <cllin7787@gmail.com>
2025-10-20 13:03:54 +1100

    Added Home view

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
+import Control from "@/components/ui/Controls";
+import { getCurrentLocation } from "@/lib/places";
+import { Mood, PriceBand } from "@/lib/types";
+import { Stack, useRouter } from "expo-router";
+import { use, useState } from "react";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function Home() {
+  const router = useRouter();
+  const [loadingLocation, setLoadingLocation] = useState(true);
+  const currentLocation = use(getCurrentLocation(setLoadingLocation));
+  const [coordinate, setCoordinate] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(currentLocation || null);
+
+  const [timeBudget, setTimeBudget] = useState(20);
+  const [price, setPrice] = useState<PriceBand>(2);
+  const [mood, setMood] = useState<Mood>("comfort");
+  const [dietary, setDietary] = useState<string[]>([]);
+
+  const onButtonPress = async () => {
+    const newCurrentLocation = await getCurrentLocation(setLoadingLocation);
+
+    if (newCurrentLocation !== coordinate) {
+      router.push({
+        pathname: "/results",
+        params: {
+          lat: newCurrentLocation?.lat ?? -33.8688,
+          lng: newCurrentLocation?.lng ?? 151.2093,
+          timeBudget,
+          price,
+          mood,
+          dietary: dietary.join(","),
+        },
+      });
+
+      setCoordinate(currentLocation || null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>🍱 MyFoodie</Text>
-      <Text>Welcome!</Text>
+      <Stack.Screen options={{ title: "My Foodie" }} />
+      <Text style={styles.title}>Get a grub nearby</Text>
+      {loadingLocation && coordinate ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Text>Location ready ✓</Text>
+      )}
+
+      <Control
+        timeBudget={timeBudget}
+        setTimeBudget={setTimeBudget}
+        price={price}
+        setPrice={setPrice}
+        mood={mood}
+        setMood={setMood}
+        dietary={dietary}
+        setDietary={setDietary}
+      />
+
+      <Button title="Help me find something bussin'" onPress={onButtonPress} />
     </View>
   );
 }
@@ -14,6 +76,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    padding: 16,
+    gap: 16,
   },
   title: {
     fontSize: 28,
